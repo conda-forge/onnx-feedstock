e852c8b9847e63de92ea4c6c4b1a4a17f396f904
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-11 11:13:46 +0100

    Use protoc from BUILD

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 export ONNX_ML=1 
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
+export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
