7259feefe857a8357aef60871b8a13999b398e35
Kerry Su <me@sshockwave.net>
2023-03-30 20:57:49 +0800

    Build shared libs instead

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
 cmake --install .setuptools-cmake-build
