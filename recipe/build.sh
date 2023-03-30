20edc86e59c9d58a3ae6ed809c56cd72e56db257
Kerry Su <me@sshockwave.net>
2023-03-30 20:34:25 +0800

    Add cmake install for build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,3 +7,4 @@ export ONNX_ML=1
 export CONDA_PREFIX="$PREFIX"
 export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
+cmake --install .setuptools-cmake-build
