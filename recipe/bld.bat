68183c7b556777a452b728641714b9328ca0cd0a
Kerry Su <me@sshockwave.net>
2023-03-31 08:48:24 +0800

    Revert windows shared build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,9 +2,8 @@
 set "ONNX_ML=1"
 set CONDA_PREFIX=%LIBRARY_PREFIX%
 set CMAKE_BUILD_TYPE=Release
-set "CMAKE_ARGS=-DBUILD_SHARED_LIBS=ON -DONNX_USE_LITE_PROTO=ON"
+set "CMAKE_ARGS=-DONNX_USE_PROTOBUF_SHARED_LIBS=ON -DProtobuf_USE_STATIC_LIBS=OFF -DONNX_USE_LITE_PROTO=ON"
 set "PYTHON_EXECUTABLE=%PYTHON%"
 set "PYTHON_LIBRARIES=%LIBRARY_LIB%"
 set USE_MSVC_STATIC_RUNTIME=0
 %PYTHON% -m pip install --no-deps --no-use-pep517 --ignore-installed --verbose .
-cmake --install .setuptools-cmake-build
