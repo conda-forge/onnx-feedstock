cc562649e2d92b014a835af72dad8ffbdd8a3ebf
Uwe L. Korn <uwe.korn@quantco.com>
2023-07-04 09:41:18 +0200

    Force C++17

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,12 @@
 
 set -euxo pipefail
 
+# Let us set the C++ Standard
+sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
+
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DCMAKE_CXX_STANDARD=17"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
 cmake --install .setuptools-cmake-build
