b103f25018fbf7f7538826cccf405fab80aac6e0
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-07-19 20:29:14 +0400

    Squashed commit of the following:
    commit e6266d3bbaf00eeb7d3130e3d6d780f4c6e590dc
    Merge: 116cece c139960
    Author: Ilya Lavrenov <ilya.lavrenov@intel.com>
    Date:   Wed Jul 19 20:18:46 2023 +0400
    
        Merge remote-tracking branch 'upstream/main' into dependencies
    
    commit 116cece6f76c772feeda8129a2bd0efae44d8d4b
    Author: Ilya Lavrenov <ilya.lavrenov@intel.com>
    Date:   Thu Apr 27 20:53:26 2023 +0400
    
        Removed excess dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,6 @@ sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DCMAKE_CXX_STANDARD=17"
+export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DONNX_USE_LITE_PROTO=ON -DONNX_USE_PROTOBUF_SHARED_LIBS=OFF -DProtobuf_USE_STATIC_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf.a -DCMAKE_CXX_STANDARD=17"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
 cmake --install .setuptools-cmake-build
