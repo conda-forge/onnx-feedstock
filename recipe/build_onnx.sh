40116588c29afd061e19b10079b79432f9e97877
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-05 11:34:26 -0500

    Update

--- recipe/build_onnx.sh
+++ recipe/build_onnx.sh
@@ -4,8 +4,9 @@ set -euxo pipefail
 # Copy over our python only cmake script
 # it is mostly just the original cmake script with everything not
 # related to python deleted
-cp ${RECIPE_DIR}/CMakeLists_python_only.txt CMakeLists.txt
+# cp ${RECIPE_DIR}/CMakeLists_python_only.txt CMakeLists.txt
 
+# This will technically rebuild the onnx librayr too......
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
