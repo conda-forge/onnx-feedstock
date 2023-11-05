c6d5f9b39e4ad2c2e47c4b110011b237a221c1dc
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-05 08:16:49 -0500

    Add libprotobuf

--- recipe/build_onnx.sh
+++ recipe/build_onnx.sh
@@ -11,4 +11,3 @@ export ONNX_ML=1
 export CONDA_PREFIX="$PREFIX"
 export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include -DCMAKE_CXX_STANDARD=17"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
-cmake --install .setuptools-cmake-build
