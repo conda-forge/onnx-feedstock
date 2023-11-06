213db3aff9c4daeb9881ab4c2ca0066d264c3a1f
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-06 07:29:19 -0500

    Try to build libonnx on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,18 @@
 @echo on
-set "ONNX_ML=1"
-set CONDA_PREFIX=%LIBRARY_PREFIX%
-set CMAKE_BUILD_TYPE=Release
+mkdir build
+cd build
 
-set "CMAKE_ARGS=%CMAKE_ARGS% -DONNX_USE_PROTOBUF_SHARED_LIBS=ON -DProtobuf_USE_STATIC_LIBS=OFF -DONNX_USE_LITE_PROTO=ON -DCMAKE_CXX_FLAGS="""/DPROTOBUF_USE_DLLS=1 /EHsc /std:c++17""""
-set "PYTHON_EXECUTABLE=%PYTHON%"
-set "PYTHON_LIBRARIES=%LIBRARY_LIB%"
-set USE_MSVC_STATIC_RUNTIME=0
-%PYTHON% -m pip install --no-deps --ignore-installed --verbose .
+set CONDA_PREFIX=%LIBRARY_PREFIX%
+cmake %CMAKE_ARGS% ^
+    -DONNX_ML=1 ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DONNX_USE_PROTOBUF_SHARED_LIBS=ON ^
+    -DProtobuf_USE_STATIC_LIBS=OFF ^
+    -DONNX_USE_LITE_PROTO=ON ^
+    -DCMAKE_CXX_FLAGS="/DPROTOBUF_USE_DLLS=1 /EHsc /std:c++17" ^
+    -DUSE_MSVC_STATIC_RUNTIME=0 ^
+    ..
+if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
+
+cmake --build . --target install --config Release
+if %ERRORLEVEL% neq 0 exit 1
