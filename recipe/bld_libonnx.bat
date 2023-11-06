9694f50e97a6e56e89891ec8b2a1c3364d264f92
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-06 07:24:57 -0500

    more defines

--- recipe/bld_libonnx.bat
+++ recipe/bld_libonnx.bat
@@ -2,12 +2,14 @@ mkdir build
 cd build
 
 cmake %CMAKE_ARGS% ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DBUILD_SHARED_LIBS=ON ^
     -DONNX_USE_PROTOBUF_SHARED_LIBS=ON ^
     -DProtobuf_USE_STATIC_LIBS=OFF ^
     -DDONNX_USE_LITE_PROTO=ON ^
     -DCMAKE_CXX_FLAGS="/DPROTOBUF_USE_DLLS=1 /EHsc /std:c++17" ^
+    -DUSE_MSVC_STATIC_RUNTIME=0 ^
     ..
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
