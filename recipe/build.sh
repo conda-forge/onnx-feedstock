e38aae69270e46fe3ff7165b6447453984e20cad
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-11 21:14:35 -0500

    Add my lib build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -euxo pipefail
+mkdir build
+cd build
 
-export ONNX_ML=1
-# build script looks at this, but not set on
-export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include -DCMAKE_CXX_STANDARD=17"
-$PYTHON -m pip install --no-deps --ignore-installed --verbose .
-cmake --install .setuptools-cmake-build
+cmake ${CMAKE_ARGS} \
+    -DONNX_ML=1 \
+    -DBUILD_SHARED_LIBS=ON \
+    -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc \
+    -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} \
+    -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include \
+    ..
+
+make -j${CPU_COUNT}
+make install
