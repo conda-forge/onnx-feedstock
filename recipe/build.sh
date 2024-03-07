504980aa91cd535c2b0b8567475ced87222bd207
Christian Bourjau <christian.bourjau@quantco.com>
2024-03-07 20:06:47 +0100

    Drop Python 3.8 support in favor of better NumPy compatibility

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include -DCMAKE_CXX_STANDARD=17"
+export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include -DCMAKE_CXX_STANDARD=17 -DNPY_TARGET_VERSION=NPY_1_19_API_VERSION"
 $PYTHON -m pip install --no-deps --ignore-installed --verbose .
 cmake --install .setuptools-cmake-build
