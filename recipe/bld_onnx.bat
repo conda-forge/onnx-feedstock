a65ec354ac27457f2c9149a5bfa8ca0523704f66
Mark Harfouche <mark.harfouche@gmail.com>
2024-03-09 18:35:58 -0500

    Update bld_onnx.bat

--- recipe/bld_onnx.bat
+++ recipe/bld_onnx.bat
@@ -1,17 +1,7 @@
 @echo on
-sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
-if %ERRORLEVEL% neq 0 exit 1
-
-REM Copy over our python only cmake script
-REM it is mostly just the original cmake script with everything not
-REM related to python deleted
-REM I can't get this to work, so technically we will be building the same package multiple times
-REM copy %RECIPE_DIR%\CMakeLists_python_only.txt CMakeLists.txt
-
 set "ONNX_ML=1"
-set CONDA_PREFIX=%LIBRARY_PREFIX%
-set CMAKE_BUILD_TYPE=Release
 
+set CMAKE_BUILD_TYPE=Release
 set "CMAKE_ARGS=%CMAKE_ARGS% -DONNX_ML=1"
 set "CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_CXX_STANDARD=17"
 set "CMAKE_ARGS=%CMAKE_ARGS% -DONNX_USE_PROTOBUF_SHARED_LIBS=ON"
@@ -21,6 +11,4 @@ set "CMAKE_ARGS=%CMAKE_ARGS% -DUSE_MSVC_STATIC_RUNTIME=0"
 
 set "PYTHON_EXECUTABLE=%PYTHON%"
 set "PYTHON_LIBRARIES=%LIBRARY_LIB%"
-set USE_MSVC_STATIC_RUNTIME=0
-%PYTHON% -m pip install --no-deps --no-use-pep517 --ignore-installed --verbose .
 %PYTHON% -m pip install --no-deps --ignore-installed --verbose .
