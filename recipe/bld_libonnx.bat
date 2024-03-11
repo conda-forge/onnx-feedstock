235fdcd339d81e72c8a7c4b12fd447817e6d0665
Mark Harfouche <mark.harfouche@gmail.com>
2024-03-11 01:58:22 -0400

    BUILD_SHARED_LIBS=ON

--- recipe/bld_libonnx.bat
+++ recipe/bld_libonnx.bat
@@ -6,9 +6,10 @@ cmake %CMAKE_ARGS% ^
     -DONNX_ML=1 ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_CXX_STANDARD=17 ^
-    -DBUILD_SHARED_LIBS=OFF ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DONNX_USE_PROTOBUF_SHARED_LIBS=ON ^
     -DProtobuf_USE_STATIC_LIBS=OFF ^
+    -DONNX_USE_LITE_PROTO=OFF ^
     -DUSE_MSVC_STATIC_RUNTIME=0 ^
     ..
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
