9589d6509147ff5d73550828fad46046253f4554
Mark Harfouche <mark.harfouche@gmail.com>
2024-11-22 13:16:48 +0530

    Refresh patches

--- recipe/bld_libonnx.bat
+++ recipe/bld_libonnx.bat
@@ -2,14 +2,14 @@
 mkdir build
 cd build
 
-cmake %CMAKE_ARGS% ^
-    -DONNX_ML=1 ^
-    -DCMAKE_CXX_STANDARD=17 ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -DONNX_USE_PROTOBUF_SHARED_LIBS=ON ^
-    -DProtobuf_USE_STATIC_LIBS=OFF ^
-    -DONNX_USE_LITE_PROTO=OFF ^
-    -DUSE_MSVC_STATIC_RUNTIME=0 ^
+cmake %CMAKE_ARGS%                      ^
+    -DONNX_ML=1                         ^
+    -DCMAKE_CXX_STANDARD=17             ^
+    -DBUILD_SHARED_LIBS=ON              ^
+    -DONNX_USE_PROTOBUF_SHARED_LIBS=ON  ^
+    -DProtobuf_USE_STATIC_LIBS=OFF      ^
+    -DONNX_USE_LITE_PROTO=OFF           ^
+    -DUSE_MSVC_STATIC_RUNTIME=0         ^
     ..
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
