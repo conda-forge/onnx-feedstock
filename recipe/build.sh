4d13919ea6ef4cc0c51b50a77f546f389b59c386
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-27 18:54:28 -0500

    Update build process for nanobind

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,13 @@ set -euxo pipefail
 export ONNX_ML=1
 # build script looks at this, but not set on
 export CONDA_PREFIX="$PREFIX"
-export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include -DCMAKE_CXX_STANDARD=17 -DNPY_TARGET_VERSION=NPY_1_19_API_VERSION -DPYBIND11_FINDPYTHON=ON -DPython_EXECUTABLE=$PYTHON"
-
-# Ensure pybind11 is searched before we run find_package(Python)
-sed -i '0,/cmake_policy(SET CMP0063 NEW)/a find_package(pybind11 REQUIRED)' CMakeLists.txt
-
+export CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON"
+export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_INCLUDE_DIR:PATH=${PREFIX}/include"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=17"
+export CMAKE_ARGS="${CMAKE_ARGS} -DNPY_TARGET_VERSION=NPY_1_19_API_VERSION"
+export CMAKE_ARGS="${CMAKE_ARGS} -DPython_EXECUTABLE=$PYTHON"
 
 # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 # Does not apply to osx-arm64 as its SDK is higher
