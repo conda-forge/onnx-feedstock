36b335e4e589187a38584951046a77080486bb7c
Christian Bourjau <christian.bourjau@quantco.com>
2026-06-16 11:15:41 +0200

    Remove reconfigure step on Windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -14,14 +14,3 @@ set "CMAKE_ARGS=%CMAKE_ARGS% -DFETCHCONTENT_FULLY_DISCONNECTED=ON"
 set USE_MSVC_STATIC_RUNTIME=0
 %PYTHON% -m pip install --no-deps --ignore-installed --verbose .
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-
-:REM Reconfigure the build to produce the shared C++ libraries without the
-:REM Python bindings, then install them. onnx's pyproject.toml sets
-:REM ONNX_INSTALL=OFF for the wheel build, so we have to flip it back on here
-:REM for `cmake --install` to emit the C++ targets and cmake config files.
-cmake -S . -B .setuptools-cmake-build %CMAKE_ARGS% -DONNX_BUILD_PYTHON=OFF -DBUILD_SHARED_LIBS=ON -DONNX_INSTALL=ON
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-cmake --build .setuptools-cmake-build
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-cmake --install .setuptools-cmake-build
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
